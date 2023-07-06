30121ef4517e27f7a39035eefd31e9e2db9bf4ad
Rayane Naboulsi <rayanenaboulsi02@gmail.com>
2023-07-06 13:24:05 +0300

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  loadedFeature='recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature; //ta n8ayer men recipe la shopping list
+  }
 }
